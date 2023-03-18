269bd67561e1cccc342f0a338bd4bd831b5420ea
Fernando Lopez <flopez00032@gmail.com>
2023-03-18 10:48:29 -0500

    sfml start

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main () 
+{
+    // cout<< "sanity check" << endl;
+    
+    
+    
+    
+    
+    // end 
+    return 0; 
+}
