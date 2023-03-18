91942f6f2d2d56692ab0ef995169e56ce8f30804
Fernando Lopez <flopez00032@gmail.com>
2023-03-18 11:01:01 -0500

    window made using sf::video mode

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,18 @@ using namespace std;
 int main () 
 {
     // cout<< "sanity check" << endl;
+
+// MAKING A WINDOW OF SIZE 800PX BY 600PX WITH A TITLE OF SFML-SANDOBX
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML-SANDBOX");
+
+
+
     
+    sf::RectangleShape rectangle (sf::Vector2f(100, 50));
+    rectangle.setPosition (200, 300);
+    rectangle.setFillColor(sf::Color::Red);
+
+    window.draw(rectangle);
     
     
     
